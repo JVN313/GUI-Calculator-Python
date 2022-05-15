e5cf57b0c970752c1f0563766eda5ff5f3fbf3f3
JVN313 <82493100+JVN313@users.noreply.github.com>
2022-05-15 09:37:45 -0400

    Added Buttons To The GUI

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from tkinter import *
 
 #window Creation / code inbetween window and window.mainloop() will apear on screen
 window = Tk()
-window.geometry("420x420") #Arguements placed in strings
+#window.geometry("420x420") #Arguements placed in strings
 window.title("Jamm's Calculator")
 window_icon = PhotoImage(file="icon.png") #Loads image, cannot be a JPG file
 window.iconphoto(True, window_icon)
 window.config(background="#808080") #config is multi-functional used to make changes such as background color.
+
+buttons = []
+for i in range(0,11):
+    buttons.append(Button(window,text=str(i)))
+    buttons[i].pack(side=LEFT)
+
 
 #Makes window visible
 window.mainloop()
--- test.py
+++ test.py
@@ -1,2 +1,1 @@
-person = (20,)
-print(person)
+
